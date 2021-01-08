85724dabcf126e473a2b6f83f48abd3ceb8d0aea
Ezgi Nazman, Ph.D <39432745+ezgicn@users.noreply.github.com>
2021-01-08 11:23:37 +0300

    review

--- LR_FLR.m
+++ LR_FLR.m
@@ -1,6 +1,6 @@
 N=50;k=1; ss=100;
-x1=normrnd(0,1,ss,1); % x'in birinci sütunu
-x2=normrnd(0,1,ss,1); % x'in ikinci sütunu
+x1=normrnd(0,1,ss,1); 
+x2=normrnd(0,1,ss,1); 
 
 X=[ones(ss,1) x1 x2]; x=[x1 x2]; Xint=ones(ss,1); Xyeniint=ones(ss,1);
 y=binornd(1, 0.10, ss, 1); 
@@ -26,8 +26,8 @@ betaLR_old=betaLR+1;
 betafirth=beta;
 betafirth_old=betafirth+1;
 
-%%%PARAMETRE TAHMÝNLERÝ, Newton-Raphson yöntemi
-%%%Lojistik regresyon yöntemi (LR)
+%%%Parameter Estimations
+%%%Logistic Regression (LR)
 while (max(abs(betaLR-betaLR_old))>0.001)
 u=X*betaLR;
 p = 1./(1+exp(-u));
@@ -39,7 +39,7 @@ end
 betaLR1(k)=betaLR(1); betaLR2(k)=betaLR(2); betaLR3(k)=betaLR(3);
 
 
-%%%Firth's Lojistik regresyon yöntemi (FLR)
+%%%Firth's Logistic Regression (FLR)
 while(max(abs(betafirth-betafirth_old))>0.001) 
 uf=X*betafirth;
 pfirth = 1./(1+exp(-uf));
@@ -54,26 +54,26 @@ betafirth1(k)=betafirth(1);
 betafirth2(k)=betafirth(2);
 betafirth3(k)=betafirth(3);
 
-%%%%LR için parametre yanlýlýðý 
+%%%%Bias Calculation for LR 
 tempbiasLR(k,1)=betaLR(1)-beta0;
 tempbiasLR(k,2)=betaLR(2)-beta1;
 tempbiasLR(k,3)=betaLR(3)-beta2;
-%%%%FLR için parametre yanlýlýðý 
+%%%%Bias Calculation for FLR
 tempbiasfirth(k,1)=betafirth(1)-beta0;
 tempbiasfirth(k,2)=betafirth(2)-beta1;
 tempbiasfirth(k,3)=betafirth(3)-beta2;
 
-%%%LR için kestirilen olasýlýk (predicted probability)
+%%%Predicted probability for LR
 ppbLR=1./(1+exp(-X*betaLR));
 
-%%%FLR için kestirilen olasýlýk 
+%%%Predicted probability for FLR
 ppbfirth=1./(1+exp(-X*betafirth));
 
-%%%LR için kestirilen olasýlýk yan (Predicted probability bias)
+%%%Predicted probability bias for LR
 probbiasLR=ppbLR-TPB; probbiasLRkare=(ppbLR-TPB).^2;
 topprobbiasLR(k)=sum(probbiasLR); topprobbiasLRkare(k)=sum(probbiasLRkare);
 
-%%%FLR için kestirilen olasýlýk yan (Predicted probability bias)
+%%%Predicted probability bias for FLR
 probbiasfirth=ppbfirth-TPB; probbiasfirthkare=(ppbfirth-TPB).^2;
 topprobbiasfirth(k)=sum(probbiasfirth); topprobbiasfirthkare(k)=sum(probbiasfirthkare);
 
@@ -89,27 +89,27 @@ check1 = isnan(tempbiasLR(k,1)); check2 = isnan(tempbiasfirth(k,1));
         end
 end
 
-%%%%LR ortalama parametre deðerleri
+%%%%Average parameter estimations
 ortparLR=[mean(betaLR1); mean(betaLR2);mean(betaLR3)]
 ortparfirth=[mean(betafirth1); mean(betafirth2);mean(betafirth3)]
 
-%%%LR için kestirilen parametre yan (predicted parameter bias)
+%%%Predicted parameter bias for LR
 meanbiasLR= mean(tempbiasLR);
 
-%%%FLR için kestirilen parametre yan (predicted parameter bias)
+%%%Predicted parameter bias for FLR
 meanbiasfirth= mean(tempbiasfirth);
 
-%%%LR için parametre yanlar (estimated parameter bias)
+%%%Estimated parameter bias for LR
 parametreyanbeta0=[meanbiasLR(1); meanbiasfirth(1)]                                    
 parametreyanbeta1=[meanbiasLR(2); meanbiasfirth(2)]                                     
 parametreyanbeta2=[meanbiasLR(3); meanbiasfirth(3)]
 
-%%%%LR için kestirilen olasýlýk yan (predicted probability bias) ve RMSE
+%%%%Predicted probability bias and Root Mean Square Error for LR
 sonprobbiasLR=sum(topprobbiasLR)/ss*N; 
 stdprobbiasLR=std(topprobbiasLR); 
 RMSELR=sqrt((sum(topprobbiasLRkare))/ss*N);
 
-%%%%FLR için kestirilen olasýlýk yan (predicted probability bias) ve RMSE
+%%%%Predicted probability bias and Root Mean Square Error for FLR
 sonprobbiasfirth=sum(topprobbiasfirth)/ss*N;
 stdprobbiasfirth=std(topprobbiasfirth);
 RMSEfirth=sqrt((sum(topprobbiasfirthkare))/ss*N);
